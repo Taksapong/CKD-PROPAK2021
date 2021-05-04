0939c9b98adffd375cf31146de8a25b60c788264
may-my-tech <39182655+Taksapong@users.noreply.github.com>
2021-05-04 21:49:14 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,403 @@
+<html>
+
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=windows-874">
+<meta name=Generator content="Microsoft Word 15 (filtered)">
+<style>
+<!--
+ /* Font Definitions */
+ @font-face
+	{font-family:"Cordia New";
+	panose-1:2 11 3 4 2 2 2 2 2 4;}
+@font-face
+	{font-family:"Cambria Math";
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:Calibri;
+	panose-1:2 15 5 2 2 2 4 3 2 4;}
+ /* Style Definitions */
+ p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin-top:0in;
+	margin-right:0in;
+	margin-bottom:8.0pt;
+	margin-left:0in;
+	line-height:107%;
+	font-size:11.0pt;
+	font-family:"Calibri",sans-serif;}
+.MsoChpDefault
+	{font-family:"Calibri",sans-serif;}
+.MsoPapDefault
+	{margin-bottom:8.0pt;
+	line-height:107%;}
+@page WordSection1
+	{size:8.5in 11.0in;
+	margin:1.0in 1.0in 1.0in 1.0in;}
+div.WordSection1
+	{page:WordSection1;}
+-->
+</style>
+
+</head>
+
+<body lang=EN-US style='word-wrap:break-word'>
+
+<div class=WordSection1>
+
+<p class=MsoNormal>
+
+<table cellpadding=0 cellspacing=0 align=left>
+ <tr>
+  <td width=0 height=1></td>
+  <td width=1></td>
+  <td width=23></td>
+  <td width=6></td>
+  <td width=128></td>
+  <td width=8></td>
+  <td width=33></td>
+  <td width=15></td>
+  <td width=14></td>
+  <td width=55></td>
+  <td width=5></td>
+  <td width=23></td>
+  <td width=26></td>
+  <td width=4></td>
+  <td width=1></td>
+  <td width=10></td>
+  <td width=4></td>
+  <td width=1></td>
+  <td width=6></td>
+  <td width=72></td>
+  <td width=13></td>
+  <td width=14></td>
+  <td width=40></td>
+  <td width=6></td>
+  <td width=2></td>
+  <td width=16></td>
+  <td width=12></td>
+  <td width=8></td>
+  <td width=14></td>
+  <td width=4></td>
+  <td width=7></td>
+  <td width=14></td>
+  <td width=43></td>
+  <td width=53></td>
+  <td width=8></td>
+  <td width=3></td>
+  <td width=4></td>
+  <td width=1></td>
+  <td width=1></td>
+  <td width=1></td>
+  <td width=6></td>
+  <td width=15></td>
+  <td width=1></td>
+ </tr>
+ <tr>
+  <td height=231></td>
+  <td colspan=36 align=left valign=top><map name=MicrosoftOfficeMap0><area
+  shape=Polygon
+  coords="283, 132, 301, 154, 315, 142, 297, 120, 283, 132" nohref><area
+  shape=Polygon
+  coords="382, 132, 400, 154, 414, 142, 396, 120, 382, 132" nohref><area
+  shape=Polygon
+  coords="475, 132, 493, 154, 507, 142, 489, 120, 475, 132" nohref><area
+  shape=Rect coords="423, 78, 487, 142" href="https://www.ckd.co.jp/en/"
+  title=""><area shape=Rect coords="327, 87, 394, 134"
+  href="https://www.youtube.com/channel/UC8b9F68PBqUzWrNCIMXBrgA" title=""><area
+  shape=Rect coords="232, 78, 296, 142"
+  href="https://th-th.facebook.com/pages.ckd" title=""></map><img border=0
+  width=696 height=231 src="index_files/image001.png"
+  usemap="#MicrosoftOfficeMap0"
+  alt="Facebook อิสระ ไอคอน ของ Squareplex Icons,Mr.JingJun: 2015,Qr code&#10;&#10;Description automatically generated,Cursor Hand Mouse - Free vector graphic on Pixabay,Cursor Hand Mouse - Free vector graphic on Pixabay,Cursor Hand Mouse - Free vector graphic on Pixabay"></td>
+ </tr>
+ <tr>
+  <td height=15></td>
+ </tr>
+ <tr>
+  <td height=1></td>
+  <td colspan=42 align=left valign=top><img width=721 height=1
+  src="index_files/image002.png"></td>
+ </tr>
+ <tr>
+  <td height=16></td>
+ </tr>
+ <tr>
+  <td height=554></td>
+  <td colspan=41 align=left valign=top><map name=MicrosoftOfficeMap1><area
+  shape=Rect coords="232, 99, 339, 177" nohref><area shape=Rect
+  coords="68, 203, 304, 398"
+  href="https://www.ckd.co.jp/kiki/en/product/detail/885/PAW" title=""><area
+  shape=Rect coords="13, 153, 343, 201"
+  href="https://www.ckd.co.jp/kiki/en/product/detail/885/PAW"><area shape=Rect
+  coords="15, 506, 345, 554"
+  href="https://www.ckd.co.jp/kiki/en/product/detail/319/"></map><img border=0
+  width=720 height=554 src="index_files/image003.png"
+  usemap="#MicrosoftOfficeMap1"
+  alt="IoT Compatible Components Series,PAW Series"></td>
+ </tr>
+ <tr>
+  <td height=1></td>
+ </tr>
+ <tr>
+  <td height=1></td>
+  <td colspan=3></td>
+  <td rowspan=2 align=left valign=top><img width=128 height=268
+  src="index_files/image004.jpg"></td>
+ </tr>
+ <tr>
+  <td height=267></td>
+  <td colspan=3></td>
+  <td></td>
+  <td colspan=35 rowspan=2 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/319/"><img border=0
+  width=539 height=276 src="index_files/image005.jpg"
+  alt="Diagram&#10;&#10;Description automatically generated" title=""></a></td>
+ </tr>
+ <tr>
+  <td height=9></td>
+ </tr>
+ <tr>
+  <td height=12></td>
+ </tr>
+ <tr>
+  <td height=1></td>
+  <td colspan=42 align=left valign=top><img width=721 height=1
+  src="index_files/image002.png"></td>
+ </tr>
+ <tr>
+  <td height=11></td>
+ </tr>
+ <tr>
+  <td height=48></td>
+  <td></td>
+  <td colspan=12 rowspan=6 align=left valign=top><img width=340 height=156
+  src="index_files/image006.png"
+  alt="Nitrogen gas extraction unit&#13;&#10;NSU series&#13;&#10;,Pneumatic components for food manufacturing process"></td>
+  <td colspan=3></td>
+  <td colspan=22 align=left valign=top><img width=342 height=48
+  src="index_files/image007.png" alt="Stepper motor drive"></td>
+ </tr>
+ <tr>
+  <td height=2></td>
+ </tr>
+ <tr>
+  <td height=27></td>
+  <td></td>
+  <td colspan=8></td>
+  <td colspan=11 align=left valign=top><img width=166 height=27
+  src="index_files/image008.png" alt="EBS-EBR Series"></td>
+ </tr>
+ <tr>
+  <td height=14></td>
+ </tr>
+ <tr>
+  <td height=8></td>
+  <td></td>
+  <td colspan=4></td>
+  <td colspan=5 rowspan=4 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/914/"><img border=0
+  width=145 height=108 src="index_files/image009.jpg"
+  alt="A picture containing sky&#10;&#10;Description automatically generated"
+  title=""></a></td>
+ </tr>
+ <tr>
+  <td height=57></td>
+  <td></td>
+  <td colspan=4></td>
+  <td colspan=5></td>
+  <td colspan=6 rowspan=3 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/915/"><img border=0
+  width=135 height=100 src="index_files/image010.jpg"
+  alt="A picture containing sky&#10;&#10;Description automatically generated"
+  title=""></a></td>
+ </tr>
+ <tr>
+  <td height=1></td>
+ </tr>
+ <tr>
+  <td height=42></td>
+  <td colspan=10 rowspan=3 align=left valign=top><img width=288 height=72
+  src="index_files/image011.png"
+  alt="Easily purifies nitrogen gas without using &#13;&#10;a cylinder or large equipment.&#13;&#10;"></td>
+ </tr>
+ <tr>
+  <td height=14></td>
+ </tr>
+ <tr>
+  <td height=16></td>
+  <td colspan=3></td>
+  <td colspan=6 rowspan=2 align=left valign=top><img width=94 height=28
+  src="index_files/image012.png" alt="FLSH Series"></td>
+  <td></td>
+  <td colspan=8 rowspan=2 align=left valign=top><img width=112 height=28
+  src="index_files/image013.png" alt="FGRC Series"></td>
+  <td colspan=3></td>
+  <td colspan=6 rowspan=2 align=left valign=top><img width=112 height=28
+  src="index_files/image014.png" alt="FLCR Series"></td>
+ </tr>
+ <tr>
+  <td height=12></td>
+ </tr>
+ <tr>
+  <td height=5></td>
+ </tr>
+ <tr>
+  <td height=1></td>
+  <td></td>
+  <td colspan=5 rowspan=8 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/110/"><img border=0
+  width=198 height=190 src="index_files/image015.jpg" title=""></a></td>
+ </tr>
+ <tr>
+  <td height=2></td>
+  <td></td>
+  <td colspan=7></td>
+  <td colspan=16 rowspan=3 align=left valign=top><map name=MicrosoftOfficeMap2><area
+  shape=Rect coords="101, 13, 223, 98"
+  href="https://www.ckd.co.jp/kiki/en/product/detail/909/FGRC" title=""><area
+  shape=Rect coords="0, 0, 102, 108"
+  href="https://www.ckd.co.jp/kiki/en/product/detail/911/FLSH" title=""></map><img
+  border=0 width=223 height=108 src="index_files/image016.png"
+  usemap="#MicrosoftOfficeMap2"></td>
+ </tr>
+ <tr>
+  <td height=25></td>
+  <td></td>
+  <td colspan=7></td>
+  <td></td>
+  <td colspan=9 rowspan=3 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/910/FLCR"><img border=0
+  width=128 height=108 src="index_files/image017.jpg"
+  alt="A picture containing electronics&#10;&#10;Description automatically generated"
+  title=""></a></td>
+ </tr>
+ <tr>
+  <td height=81></td>
+  <td></td>
+  <td></td>
+  <td colspan=4 rowspan=4 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/110/"><img border=0
+  width=97 height=160 src="index_files/image018.jpg" title=""></a></td>
+ </tr>
+ <tr>
+  <td height=2></td>
+ </tr>
+ <tr>
+  <td height=6></td>
+ </tr>
+ <tr>
+  <td height=71></td>
+  <td></td>
+  <td></td>
+  <td colspan=3></td>
+  <td colspan=23 rowspan=4 align=left valign=top><img width=355 height=93
+  src="index_files/image019.jpg"
+  alt="Diagram&#10;&#10;Description automatically generated"></td>
+ </tr>
+ <tr>
+  <td height=2></td>
+ </tr>
+ <tr>
+  <td height=16></td>
+ </tr>
+ <tr>
+  <td height=4></td>
+  <td></td>
+  <td colspan=11 rowspan=7 align=left valign=top><map name=MicrosoftOfficeMap3><area
+  shape=Rect coords="178, 45, 332, 98" nohref><area shape=Rect
+  coords="11, 99, 153, 204"
+  href="https://www.ckd.co.jp/kiki/en/product/detail/343/" title=""><area
+  shape=Rect coords="178, 94, 336, 199"
+  href="https://www.ckd.co.jp/kiki/en/product/detail/342/" title=""></map><img
+  border=0 width=336 height=204 src="index_files/image020.png"
+  usemap="#MicrosoftOfficeMap3"
+  alt="Motorless electric actuator ,ETV Series,ETS Series"></td>
+ </tr>
+ <tr>
+  <td height=41></td>
+ </tr>
+ <tr>
+  <td height=53></td>
+  <td></td>
+  <td colspan=4></td>
+  <td colspan=8 align=left valign=top><img width=154 height=53
+  src="index_files/image021.png" alt="ECS Series"></td>
+  <td colspan=2></td>
+  <td colspan=9 align=left valign=top><img width=154 height=53
+  src="index_files/image022.png" alt="ECV Series"></td>
+ </tr>
+ <tr>
+  <td height=7></td>
+ </tr>
+ <tr>
+  <td height=2></td>
+  <td></td>
+  <td colspan=6></td>
+  <td colspan=5 rowspan=2 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/341/"><img border=0
+  width=145 height=97 src="index_files/image023.jpg" title=""></a></td>
+ </tr>
+ <tr>
+  <td height=95></td>
+  <td></td>
+  <td colspan=6></td>
+  <td colspan=2></td>
+  <td colspan=14 rowspan=4 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/340/"><img border=0
+  width=173 height=105 src="index_files/image024.jpg" title=""></a></td>
+ </tr>
+ <tr>
+  <td height=2></td>
+ </tr>
+ <tr>
+  <td height=5></td>
+ </tr>
+ <tr>
+  <td height=3></td>
+  <td></td>
+  <td colspan=14 rowspan=2 align=left valign=top><img width=351 height=48
+  src="index_files/image025.png" alt="Image Processing Visual Programming Tool"></td>
+ </tr>
+ <tr>
+  <td height=45></td>
+ </tr>
+ <tr>
+  <td height=5></td>
+ </tr>
+ <tr>
+  <td height=172></td>
+  <td colspan=2></td>
+  <td colspan=6 align=left valign=top><a
+  href="https://www.ckd.co.jp/kiki/en/product/detail/973/Facilea(AES)?t=list&amp;cid=178&amp;sid=0"><img
+  border=0 width=204 height=172 src="index_files/image026.jpg"
+  alt="A picture containing text, electronics, computer, screenshot&#10;&#10;Description automatically generated"
+  title=""></a></td>
+  <td></td>
+  <td colspan=25 align=left valign=top><img width=406 height=172
+  src="index_files/image027.jpg"
+  alt="Diagram, text&#10;&#10;Description automatically generated with medium confidence"></td>
+ </tr>
+ <tr>
+  <td height=13></td>
+ </tr>
+ <tr>
+  <td height=1></td>
+  <td colspan=42 align=left valign=top><img width=721 height=1
+  src="index_files/image002.png"></td>
+ </tr>
+ <tr>
+  <td height=10></td>
+ </tr>
+ <tr>
+  <td height=52></td>
+  <td colspan=41 align=left valign=top><img width=720 height=52
+  src="index_files/image028.png"></td>
+ </tr>
+</table>
+
+ &nbsp;</p>
+
+</div>
+
+</body>
+
+</html>
